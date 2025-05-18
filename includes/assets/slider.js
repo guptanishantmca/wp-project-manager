19250d6ed9074ab6b74eecca365bf191ea8eaa1f
IIPN Solutions <guptanishantmca@gmail.com>
2025-05-18 17:03:46 +0530

    updates

--- includes/assets/slider.js
+++ includes/assets/slider.js
@@ -1,3 +1,14 @@
 jQuery(document).ready(function($) {
     $('.project-slider').slick();
 });
+
+
+jQuery(document).ready(function($) {
+    // Optional Fancybox init if you want to customize
+    Fancybox.bind('[data-fancybox="gallery"]', {
+        Toolbar: true,
+        Thumbs: {
+            autoStart: false,
+        },
+    });
+});
